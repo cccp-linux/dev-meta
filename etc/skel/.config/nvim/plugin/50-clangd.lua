df2ec161b6e0a60ef6cc1e40fccb1036751a51d0
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-01-30 19:38:40 -0500

    Work without nvim-lspconfig

--- etc/skel/.config/nvim/plugin/50-clangd.lua
+++ etc/skel/.config/nvim/plugin/50-clangd.lua
@@ -1,4 +1,19 @@
-local prev_on_attach = vim.tbl_get(vim.lsp.config, "clangd", "on_attach")
+local function switch_source_header(client, bufnr)
+    local method_name = "textDocument/switchSourceHeader"
+    if not client or not client:supports_method(method_name) then
+        return vim.notify(("method %s is not supported by any servers active on the current buffer"):format(method_name))
+    end
+
+    local params = vim.lsp.util.make_text_document_params(bufnr)
+    client:request(method_name, params, function(err, result)
+        if err then error(tostring(err)) end
+        if not result then
+            vim.notify("corresponding file cannot be determined")
+            return
+        end
+        vim.cmd.edit(vim.uri_to_fname(result))
+    end, bufnr)
+end
 
 vim.lsp.config("clangd", {
     cmd = {"clangd", "--compile-commands-dir=build", "--header-insertion=never"},
@@ -8,10 +23,15 @@ vim.lsp.config("clangd", {
         "configure.ac",
         ".git",
     },
+    capabilities = { textDocument = { completion = {
+        editsNearCursor = true,
+    }}},
+
     on_attach = function(client, bufnr)
-        if prev_on_attach then
-            prev_on_attach(client, bufnr)
-        end
+        vim.api.nvim_buf_create_user_command(bufnr, "LspClangdSwitchSourceHeader",
+            function() switch_source_header(client, bufnr) end,
+            {desc = "Switch between source/header"}
+        )
         vim.keymap.set("n", "gh", "<cmd>LspClangdSwitchSourceHeader<cr>", {buffer = bufnr})
     end
 })
--- etc/skel/.config/nvim/plugin/50-pylsp.lua
+++ etc/skel/.config/nvim/plugin/50-pylsp.lua
@@ -1,1 +1,14 @@
+vim.lsp.config("pylsp", {
+    cmd = {"pylsp"},
+    filetypes = {"python"},
+    root_markers = {
+        ".git",
+        "Pipfile",
+        "pyproject.toml",
+        "requirements.txt",
+        "setup.cfg",
+        "setup.py",
+    },
+})
+
 vim.lsp.enable("pylsp")
