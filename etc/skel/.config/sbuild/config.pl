8dfd56282a26f562735a209cd9f255b9fb1f7f25
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-21 11:44:33 -0400

    Make sbuild auto-create/update chroots

--- etc/skel/.config/sbuild/config.pl
+++ etc/skel/.config/sbuild/config.pl
@@ -10,4 +10,14 @@ $external_commands = {
 };
 $run_lintian = 0;
 
+my @ubuntu_distros = split(/\n/, `ubuntu-distro-info --all`);
+$unshare_mmdebstrap_extra_args = [
+    "*" => [ "--verbose",
+        "--customize-hook=chroot \$1 update-ccache-symlinks",
+        "--include=aptitude,ca-certificates,ccache,eatmydata",
+    ],
+    map { $_ => [ "--components=main,universe" ] } @ubuntu_distros,
+];
+$unshare_mmdebstrap_keep_tarball = 1;
+
 1;
