b0315a40f7e8c773cce689d4c5ef6c1774a71f83
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-21 16:20:00 -0400

    sbuild/config.pl: fix ccache

--- etc/skel/.config/sbuild/config.pl
+++ etc/skel/.config/sbuild/config.pl
@@ -1,6 +1,7 @@
 # config.pl
 
 # $build_dep_resolver = "aptitude";
+$build_environment = { "CCACHE_DIR" => "/build/ccache" };
 $check_space = 0;
 $chroot_mode = "unshare";
 
@@ -8,7 +9,22 @@ $external_commands = {
     "build-deps-failed-commands" => [ [ "%SBUILD_SHELL" ] ],
     "build-failed-commands" => [ [ "%SBUILD_SHELL" ] ],
 };
+$path = "/usr/lib/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games";
 $run_lintian = 0;
+
+##########
+# Enable access to the ccache dir from inside the chroot:
+#
+#   rid=$(grep `whoami` /etc/subgid | cut -d: -f2)
+#   cid=$((rid + 999))
+#
+#   setfacl -m u:${rid}:x -m u:${cid}:x "$HOME" "$HOME/.cache"
+#   setfacl -m u:${rid}:rwx -m u:${cid}:rwx "$HOME/.cache/ccache"
+#
+##########
+$unshare_bind_mounts = [
+    { directory => "$HOME/.cache/ccache", mountpoint => "/build/ccache" }
+];
 
 my @ubuntu_distros = split(/\n/, `ubuntu-distro-info --all`);
 $unshare_mmdebstrap_extra_args = [
